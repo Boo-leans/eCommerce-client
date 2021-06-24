fab30ce819fd8b4cc59d855a61abef265840db02
MiketoCoding <le.michael.v@gmail.com>
2021-06-24 14:42:20 -0400

    Add axios calls to shoppingCart
    Delete, Index Axios calls added

--- src/api/shoppingCart.js
+++ src/api/shoppingCart.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 import apiUrl from '../apiConfig'
 
-export const createCart = () => {
-  return axios({
-    method: 'POST',
-    url: apiUrl + '/shoppingCart'
-  })
-}
+// export const createCart = () => {
+//   return axios({
+//     method: 'POST',
+//     url: apiUrl + '/shoppingCart'
+//   })
+// }
 
+// add/Update purchase history
 export const productCreate = (user, item) => {
   return axios({
     method: 'PATCH',
@@ -16,5 +17,22 @@ export const productCreate = (user, item) => {
       'Authorization': `Bearer ${user.token}`
     },
     data: { products: item }
+  })
+}
+
+// delete (refund) purchased product
+export const productRefund = (user, itemId) => {
+  return axios({
+    url: apiUrl + '/purchases/' + itemId,
+    method: 'DELETE',
+    headers: { 'Authorization': `Bearer ${user.token}` }
+  })
+}
+
+// index purchase history
+export const productIndex = (user) => {
+  return axios({
+    url: apiUrl + '/purchases',
+    headers: { 'Authorization': `Bearer ${user.toekn}` }
   })
 }
