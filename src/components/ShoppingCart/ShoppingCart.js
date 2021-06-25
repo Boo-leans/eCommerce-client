28578b1aa515d4c91df804aa009692789623e77c
MiketoCoding <le.michael.v@gmail.com>
2021-06-25 11:41:09 -0400

    Add code to remove empty values from shopping cart
    Now does not display empty object in shopping cart. Prior error was
    store.product would push an empty product whenever we go to home page

--- src/components/ShoppingCart/ShoppingCart.js
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -1,19 +1,31 @@
 import React, { Fragment } from 'react'
 import Card from 'react-bootstrap/Card'
-// import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button'
 import store from './../../store'
-
+store.product = []
 const ShoppingCart = () => {
   const cartList = store.product
   console.log('Current shoping cart list', cartList)
 
-  const cartCards = cartList.map((product) => {
+  // useEffect(() => {
+  //   store.product.push(item)
+  //   console.log(store)
+  // }, [item])
+
+  const cartCards = cartList.filter(item => item.name !== '').map((product) => {
     return (
       <Card style={{ width: '18rem' }} key={cartList.indexOf(product)}>
         {/* <Card.Img variant='top' src={product.backgroundUrl} /> */}
         <Card.Body>
           <Card.Title>{product.name}</Card.Title>
           <Card.Text>{product.price}</Card.Text>
+
+          <Button
+            name="removeItem"
+            // onClick={() =>
+            //   setItem({ name: product.name, price: product.price })}
+            variant="secondary">Remove Item
+          </Button>
 
         </Card.Body>
       </Card>
@@ -24,6 +36,7 @@ const ShoppingCart = () => {
     <Fragment>
       <h2>Welcome to Shopping Cart</h2>
       {cartCards}
+      <Button>Checkout</Button>
     </Fragment>
   )
 }
